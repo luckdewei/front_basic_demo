43d14ae9d9ef02782986ed77edb7d5848bb1c6ce
weiwenhao <rihime@liyuans-MacBook-Pro.local>
2025-04-11 21:09:02 +0800

    更改promise写法，增加基础then方法

--- javascript_demo/myPromise/MyPromise.js
+++ javascript_demo/myPromise/MyPromise.js
@@ -1,41 +1,56 @@
-class MyPromise {
-  static PENDING = 'pending'
-  static FULFILLED = 'fulfilled'
-  static REJECTED = 'rejected'
-  constructor(func) {
-    // 初始化值
-    this.initValue()
-    // 初始化this指向
-    this.initBind()
-    try {
-      func(this.resolve, this.reject)
-    } catch (error) {
-      this.reject(error)
-    }
-  }
-  
-  initBind() {
-    this.resolve = this.resolve.bind(this)
-    this.reject = this.reject.bind(this)
-  }
+// 定义三种状态
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
 
-  initValue() {
-    // 初始状态
-    this.PromiseStatus = MyPromise.PENDING
-    // 终值
-    this.PromiseResult = undefined
-  }
-  resolve(value) {
-    if (this.PromiseStatus === MyPromise.PENDING) {
-      this.PromiseStatus = MyPromise.FULFILLED
-      this.PromiseResult = value
+class MyPromise {
+  constructor(executor) {
+    this.status = PENDING; // 初始化状态
+    this.value = undefined; // 成功的值
+    this.reason = undefined; // 失败的原因
+    this.onFulfilledCallbacks = []; // 成功的回调
+    this.onRejectedCallbacks = []; // 失败的回调
+
+    // 定义resolve函数
+    const resolve = (value) => {
+      if (this.status === PENDING) {
+        this.status = FULFILLED;
+        this.value = value;
+        this.onFulfilledCallbacks.forEach(callback => callback());
+      }
+    }
+    // 定义reject函数
+    const reject = (reason) => {
+      if (this.status === PENDING) {
+        this.status = REJECTED;
+        this.reason = reason;
+        this.onRejectedCallbacks.forEach(callback => callback());
+      }
+    }
+
+    try {
+      // 立即执行executor函数
+      executor(resolve, reject);
+    } catch (error) {
+      // 如果executor函数执行出错，直接reject
+      reject(error);
     }
   }
-  
-  reject(reason) {
-    if (this.PromiseStatus === MyPromise.PENDING) {
-      this.PromiseStatus = MyPromise.REJECTED
-      this.PromiseResult = reason
+  // then方法
+  then(onFulfilled, onRejected) {
+    if (this.status === FULFILLED) {
+      onFulfilled(this.value);
+    }
+    if (this.status === REJECTED) {
+      onRejected(this.reason);
+    }
+    if (this.status === PENDING) {
+      this.onFulfilledCallbacks.push(() => {
+        onFulfilled(this.value);
+      });
+      this.onRejectedCallbacks.push(() => {
+        onRejected(this.reason);
+      });
     }
   }
 }
