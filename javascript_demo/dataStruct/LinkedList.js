4d384f31b5518689f459381aa1ae1a61d451d863
weiwenhao <weiwenhao-szgx@bestpay.com.cn>
2025-04-22 16:22:32 +0800

    feat: finish 单链表

--- javascript_demo/dataStruct/LinkedList.js
+++ javascript_demo/dataStruct/LinkedList.js
@@ -1,4 +1,4 @@
-// https://github.com/trekhleb/javascript-algorithms/blob/master/src/data-structures/linked-list/README.zh-CN.md
+// 单链表
 
 class Node {
   constructor(value) {
@@ -70,5 +70,65 @@ class LinkedList {
     temp.next = null;
     return temp;
   }
+  // 获取指定位置的元素
+   get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let temp = this.head;
+    for (let i = 0; i < index; i++) {
+      temp = temp.next;
+    }
+    return temp;
+   }
+   // 修改指定位置的元素
+   set(index, value) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+   }
+   // 在指定位置插入元素
+   insert(index, value) {
+     if (index === 0) return this.unshift(value);
+     if (index === this.length) return this.push(value);
+     if (index < 0 || index > this.length) return false;
+     const newNode = new Node(value);
+     const temp = this.get(index - 1);
+     newNode.next = temp.next;
+     temp.next = newNode;
+     this.length++;
+     return true;
+   }
+
+   // 删除指定位置的元素
+   remove(index) {
+     if (index === 0) return this.shift();
+     if (index === this.length - 1) return this.pop();
+     if (index < 0 || index >= this.length) return undefined;
+     const temp = this.get(index - 1);
+     const removed = temp.next;
+     temp.next = temp.next.next;
+     this.length--;
+     return removed;
+   }
+
+   // 翻转链表
+   reverse() {
+    // 1. 首尾交换
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    // 2. 中间元素翻转
+    let next = temp.next;
+    let prev = null; // 设置一个变量，用来存储上一个元素
+    for (let i = 0; i < this.length; i++) {
+      next = temp.next;
+      temp.next = prev;
+      prev = temp;
+      temp = next;
+    }
+    return this;
+   }
 }
 
