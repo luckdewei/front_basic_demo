0ec57cf685611c192f127bfc9e32c575ff98f6ff
weiwenhao <wwh120105150@163.com>
2025-06-11 21:30:19 +0800

    update

--- javascript_demo/practice-demo/238.js
+++ javascript_demo/practice-demo/238.js
@@ -1,19 +1,19 @@
 
 
 
-// var productExceptSelf = function(nums) {
-//   let len = nums.length
-//   let ans = new Array(len).fill(1)
-//   let i=0,j=0;
-//   while(i<len){
-//     ans[i]*= i ==j ? 1: nums[j]
-//     j++
-//     if (j==len) {
-//       i++, j=0
-//     }
-//   }
-//   return ans
-// };
+var productExceptSelf = function(nums) {
+  let len = nums.length
+  let ans = new Array(len).fill(1)
+  let i=0,j=0;
+  while(i<len){
+    ans[i]*= i ==j ? 1: nums[j]
+    j++
+    if (j==len) {
+      i++, j=0
+    }
+  }
+  return ans
+};
 
 var productExceptSelf = function(nums) {
 
